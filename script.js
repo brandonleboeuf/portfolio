24677bad1bafc23d7603043c71396ebd9ed87d53
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-07-17 12:08:30 -0700

    jarasic park resources added

---
+++ script.js
@@ -0,0 +1,16 @@
+// jarasic park password denial gif
+{
+  /* <iframe src='https://gfycat.com/ifr/LikelyWhichHarrier' frameborder='0' scrolling='no' allowfullscreen width='640' height='404'></iframe><p> <a href="https://gfycat.com/likelywhichharrier">via Gfycat</a></p> */
+}
+
+// script taken from https://github.com/MarkHjorth/nedry
+
+{
+  /* <script>
+  ahahah = new Audio("./styles/jarasicPark/ahahah.mp3"); 
+  ahahah.addEventListener('ended', function() {
+    this.play();
+  }, false);
+  ahahah.play();
+</script> */
+}
