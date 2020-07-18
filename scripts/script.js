c50f4d80b65c4bf64a418189d0af15691e16611d
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-07-18 16:15:11 -0700

    big learning day" git push

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@
 </script> */
 }
 
+/*
 // creat toggel access hidden pages if password is valid
 
 const password = document.getElementById("passwordInput");
@@ -28,5 +29,30 @@ const handleInput = function (e) {
   ) {
   }
 };
+ */
 
-console.log(password);
+// img gallery
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const galleryImgs = document.querySelectorAll(".gallery-img ");
+let currentlySelected = 0;
+
+prevBtn.addEventListener("click", function () {
+  galleryImgs[currentlySelected].classList.remove("active");
+  currentlySelected--;
+  galleryImgs[currentlySelected].classList.add("active");
+  nextBtn.disabled = false;
+  if (currentlySelected === 0) {
+    prevBtn.disabled = true;
+  }
+});
+
+nextBtn.addEventListener("click", function () {
+  galleryImgs[currentlySelected].classList.remove("active");
+  currentlySelected++;
+  galleryImgs[currentlySelected].classList.add("active");
+  prevBtn.disabled = false;
+  if (galleryImgs.length === currentlySelected + 1) {
+    nextBtn.disabled = true;
+  }
+});
