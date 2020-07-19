177852838563b79e8516fd4f0fc9bf6240b29407
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-07-19 15:05:37 -0700

    added default for incorrect passwords

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,3 @@
-// jarasic park password denial gif
-{
-  /* <iframe src='https://gfycat.com/ifr/LikelyWhichHarrier' frameborder='0' scrolling='no' allowfullscreen width='640' height='404'></iframe><p> <a href="https://gfycat.com/likelywhichharrier">via Gfycat</a></p> */
-}
-
-// script taken from https://github.com/MarkHjorth/nedry
-
-//    <script>
-//   ahahah = new Audio("./styles/jarasicPark/ahahah.mp3");
-//   ahahah.addEventListener('ended', function() {
-//     this.play();
-//   }, false);
-//   ahahah.play();
-// </script>
-
 // creat toggel access hidden pages if password is valid
 
 const input = document.querySelector("input");
@@ -25,24 +10,27 @@ let isSelected = document.getElementById("resume");
 
 function changePage(e) {
   let pw = e.target.value.toLowerCase();
-  let prev = isSelected;
 
   if (
     pw === "d&d" ||
+    pw === "dnd" ||
     pw === "dungeons and dragons" ||
     pw === "dungeons & dragons"
   ) {
-    // class change
-    isSelected.classList.add("hidden");
-    isSelected = document.getElementById("d_and_d");
-    isSelected.classList.remove("hidden");
-  } else if (pw === "resume") {
-    isSelected.classList.add("hidden");
-    isSelected = document.getElementById("resume");
-    isSelected.classList.remove("hidden");
+    isSelected.classList.add("hidden"); // Hides current page
+    isSelected = document.getElementById("d_and_d"); // Sets new page as isSelected
+    isSelected.classList.remove("hidden"); // Removes .hidden class
   } else if (pw === "donut") {
     isSelected.classList.add("hidden");
     isSelected = document.getElementById("donut");
+    isSelected.classList.remove("hidden");
+  } else if (pw === "jurassic park") {
+    isSelected.classList.add("hidden");
+    isSelected = document.getElementById("jurassicPark");
+    isSelected.classList.remove("hidden");
+  } else {
+    isSelected.classList.add("hidden");
+    isSelected = document.getElementById("resume");
     isSelected.classList.remove("hidden");
   }
 }
