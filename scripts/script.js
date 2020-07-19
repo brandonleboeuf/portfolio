0a16f7fda6ee2d46bcefc15e0f48083554fa33d1
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-07-19 11:42:40 -0700

    added swiper gallery to index.html

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,6 @@
 </script> */
 }
 
-/*
 // creat toggel access hidden pages if password is valid
 
 const password = document.getElementById("passwordInput");
@@ -29,30 +28,3 @@ const handleInput = function (e) {
   ) {
   }
 };
- */
-
-// img gallery
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-const galleryImgs = document.querySelectorAll(".gallery-img ");
-let currentlySelected = 0;
-
-prevBtn.addEventListener("click", function () {
-  galleryImgs[currentlySelected].classList.remove("active");
-  currentlySelected--;
-  galleryImgs[currentlySelected].classList.add("active");
-  nextBtn.disabled = false;
-  if (currentlySelected === 0) {
-    prevBtn.disabled = true;
-  }
-});
-
-nextBtn.addEventListener("click", function () {
-  galleryImgs[currentlySelected].classList.remove("active");
-  currentlySelected++;
-  galleryImgs[currentlySelected].classList.add("active");
-  prevBtn.disabled = false;
-  if (galleryImgs.length === currentlySelected + 1) {
-    nextBtn.disabled = true;
-  }
-});
