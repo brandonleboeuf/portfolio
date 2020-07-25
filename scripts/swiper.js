e27945a9375bc1ae55dc5f9db2f13670de6f5e99
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-07-25 10:58:45 -0700

    updated swiper import

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,4 +1,23 @@
-new Swiper(".swiper-container", {
+// import { Swiper, SwiperOptions } from "swiper";
+// Import Swiper and modules
+import { Swiper, Navigation, Pagination, Scrollbar, EffectCube } from "swiper";
+
+// Install modules
+Swiper.use([Navigation, Pagination, Scrollbar, EffectCube]);
+
+// Now you can use Swiper
+// let swiper = new Swiper(".swiper-container", {
+//   speed: 400,
+//   effect: "cube",
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   // ...
+// });
+
+let swiper = new Swiper(".swiper-container", {
   speed: 400,
   spaceBetween: 100,
   effect: "cube",
