775cde640a3e0f0ec7b95eb2d21d93735a5d0049
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-09-03 14:31:26 -0700

    fix JS4

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -11,16 +11,15 @@ button.addEventListener('click', toggleBtn)
 
 function checkWindowSize() {
   if (window.matchMedia("(max-width: 750px)").matches) {
-    
-    createHeadroom()
-    // IF NOT MOBILE: Deactivates bubbling for mobile menu
-    menuBtns.removeEventListener('click', toggleBtn, true)
-
-  } else {
-    
     // IF MOBILE: Initiates bubbling to close menu
     menuBtns.addEventListener('click', toggleBtn, true)
     toggleBtn();
+
+  } else {
+    createHeadroom()
+    // IF NOT MOBILE: Deactivates bubbling for mobile menu
+    menuBtns.removeEventListener('click', toggleBtn, true)
+    
   }
 }
 
